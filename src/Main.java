c5be9af19b0b543e00357914a6c1f7a491558859
opgeho <opgeho@gmail.com>
2024-10-03 14:03:04 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        String partyAfil = "";
+
+        System.out.println("Enter your party affiliation [R,D,I] : ");
+        partyAfil = in.nextLine();
+
+        if(partyAfil.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant!");
+        } else if (partyAfil.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a democratic donkey!");
+        } else if (partyAfil.equalsIgnoreCase("I"))
+        {
+            System.out.println("you get an independant Man!");
+        }
+        else
+        {
+            System.out.println("Your party " + partyAfil + " is not a recognized party.");
+        }
     }
 }
